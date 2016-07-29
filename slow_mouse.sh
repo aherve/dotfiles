3e173b39f930c9de583e9a7cc3323657c5533e28
Aurélien Hervé <mail@aurelien-herve.com>
2016-07-29 10:34:46 +0200

    update

--- slow_mouse.sh
+++ slow_mouse.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
 ids=$(xinput --list | awk -v search="Logitech Gaming Mouse" \
-    '$0 ~ search {match($0, /id=[0-9]+/);\
-                  if (RSTART) \
-                    print substr($0, RSTART+3, RLENGTH-3)\
-                 }'\
-     )
+  '$0 ~ search {match($0, /id=[0-9]+/);\
+  if (RSTART) \
+    print substr($0, RSTART+3, RLENGTH-3)\
+  }'\
+  )
 
 for i in $ids
 do
-    xinput set-prop $i 'Device Accel Constant Deceleration' 5
+  xinput set-prop $i 'Device Accel Constant Deceleration' 5
+done
+
+# to deactivate trackpad : xinput set-prop 11 '133' 0 (11 is device id, 133 is 'Device enabled')
+
+ids=$(xinput --list | awk -v search="USB Optical Mouse" \
+  '$0 ~ search {match($0, /id=[0-9]+/);\
+  if (RSTART) \
+    print substr($0, RSTART+3, RLENGTH-3)\
+  }'\
+  )
+
+for i in $ids
+do
+  xinput set-prop $i 'Device Accel Constant Deceleration' 2
 done
 
 # to deactivate trackpad : xinput set-prop 11 '133' 0 (11 is device id, 133 is 'Device enabled')
