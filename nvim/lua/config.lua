663a062381bed95a5d06f96e9acd197cc6088caf
Aurélien Hervé <mail@aurelien-herve.com>
2025-06-04 11:28:13 +0200

    some formatting

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -15,47 +15,34 @@ vim.opt.mouse = "n" -- Use mouse (all)
 -- commenting behavior
 vim.opt.formatoptions:append("ln") -- See :h 'formatoptions' :)
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
 })
 
-vim.opt.scrolloff = 4     -- When moving vertical, start scrolling 4 lines before reaching bottom
+vim.opt.scrolloff = 4 -- When moving vertical, start scrolling 4 lines before reaching bottom
 vim.opt.expandtab = true
 vim.opt.autoindent = true -- Indent (based on above line) when adding a line
 vim.opt.linebreak = true
-vim.opt.tabstop = 2       -- A tab is 2 spaces
-vim.opt.softtabstop = 2   -- See 2 spaces per tab
-vim.opt.shiftwidth = 2    -- Indent is 2
-vim.opt.number = true     -- Show lines
-vim.opt.showmode = true   -- Show mode in status (insertion, visual...)
-vim.opt.showcmd = true    -- Show beginning of normal commands (try d and see at bottom-right)
-vim.opt.ruler = true      -- Show line,col in statusbar
+vim.opt.tabstop = 2 -- A tab is 2 spaces
+vim.opt.softtabstop = 2 -- See 2 spaces per tab
+vim.opt.shiftwidth = 2 -- Indent is 2
+vim.opt.number = true -- Show lines
+vim.opt.showmode = true -- Show mode in status (insertion, visual...)
+vim.opt.showcmd = true -- Show beginning of normal commands (try d and see at bottom-right)
+vim.opt.ruler = true -- Show line,col in statusbar
 
 -- Windows
 vim.opt.splitright = true -- Vsplit at right
 -- vim.opt.previewheight = 8 -- Height of preview menu (Omni-completion)
 
 -- Search
-vim.opt.wrapscan = true  -- Continue to top after reaching bottom
+vim.opt.wrapscan = true -- Continue to top after reaching bottom
 vim.opt.hlsearch = false -- Highlight search
 vim.opt.incsearch = true -- See results of search step by step
 vim.opt.showmatch = true -- Parenthesis matches
-vim.opt.matchtime = 2    -- Show new matching parenthesis for 2/10th of sec
-
-
---[[
-   [-- Use home directory for swap files
-   [local swap_dir = vim.fn.expand("$HOME/.config/nvim/tmp/swap")
-   [
-   [if vim.fn.isdirectory(swap_dir) == 0 then
-   [  vim.fn.mkdir(swap_dir, "p", 0700)
-   [end
-   [
-   [vim.opt.directory = swap_dir
-   ]]
-
+vim.opt.matchtime = 2 -- Show new matching parenthesis for 2/10th of sec
 
 -- auto-folding
 -- vim.opt.foldenable = true
@@ -63,37 +50,36 @@ vim.opt.foldenable = false
 vim.opt.foldmethod = "syntax"
 vim.opt.foldnestmax = 2
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    if vim.fn.line("$") < 60 then
-      vim.opt.foldlevel = 1000
-    end
-  end,
+	pattern = "*",
+	callback = function()
+		if vim.fn.line("$") < 60 then
+			vim.opt.foldlevel = 1000
+		end
+	end,
 })
 
+vim.g.go_def_mode = "gopls"
+vim.g.go_info_mode = "gopls"
 
-vim.g.go_def_mode = 'gopls'
-vim.g.go_info_mode = 'gopls'
-
-vim.g.closetag_filenames = '*.html,*.jsx,*.tsx'
+vim.g.closetag_filenames = "*.html,*.jsx,*.tsx"
 vim.g.closetag_regions = {
-  ['typescript.tsx'] = 'jsxRegion,tsxRegion',
-  ['javascript.jsx'] = 'jsxRegion',
+	["typescript.tsx"] = "jsxRegion,tsxRegion",
+	["javascript.jsx"] = "jsxRegion",
 }
 
 -- Remap copilot complete (tab is used by coc)
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
-vim.keymap.set('i', '<C-k>', function()
-  return vim.fn['copilot#Accept']()
+vim.keymap.set("i", "<C-k>", function()
+	return vim.fn["copilot#Accept"]()
 end, { expr = true, silent = true, replace_keycodes = false })
 
 -- Mergetool settings
-vim.g.mergetool_layout = 'mr'
-vim.g.mergetool_prefer_revision = 'local'
+vim.g.mergetool_layout = "mr"
+vim.g.mergetool_prefer_revision = "local"
 
 -- Python host program
-vim.g.python3_host_prog = vim.fn.expand('~/.pyenv/shims/python3')
+vim.g.python3_host_prog = vim.fn.expand("~/.pyenv/shims/python3")
 
 -- git gutter: always display signs column (avoid flickering)
 vim.opt.signcolumn = "yes"
--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -1,188 +1,192 @@
 -- Install Lazy.nvim if not already installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugin configuration
 require("lazy").setup({
-  -- Colorscheme
-  require("plugins.theme"),
-  require('plugins.telescope'),
-  require('plugins.vim-go'),
-  require('plugins.codecompanion'),
-  require('plugins.mason'),
-  require('plugins.conform'),
+	-- Colorscheme
+	require("plugins.theme"),
+	require("plugins.telescope"),
+	require("plugins.vim-go"),
+	require("plugins.codecompanion"),
+	require("plugins.mason"),
+	require("plugins.conform"),
 
-  { 'hrsh7th/nvim-cmp' },     -- Autocomplete engine
-  { 'hrsh7th/cmp-nvim-lsp' }, -- Completion source for LSP
-  {
-    'L3MON4D3/LuaSnip',
-    build = "make install_jsregexp",
-    dependencies = { "rafamadriz/friendly-snippets" },
-  }, -- Snippet engine
+	{ "hrsh7th/nvim-cmp" }, -- Autocomplete engine
+	{ "hrsh7th/cmp-nvim-lsp" }, -- Completion source for LSP
+	{
+		"L3MON4D3/LuaSnip",
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	}, -- Snippet engine
 
-
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("nvim-tree").setup {}
-    end,
-  },
-  "Lokaltog/vim-easymotion",
-  "airblade/vim-gitgutter",
-  "alvan/vim-closetag",
-  "artoj/pgn-syntax-vim",
-  "blueyed/vim-diminactive",
-  "chrisbra/csv.vim",
-  "derekwyatt/vim-scala",
-  "ekalinin/Dockerfile.vim",
-  "github/copilot.vim",
-  "hashivim/vim-terraform",
-  "jiangmiao/auto-pairs",
-  "jparise/vim-graphql",
-  { "junegunn/fzf", build = function() vim.fn["fzf#install"]() end },
-  "junegunn/fzf.vim",
-  "jxnblk/vim-mdx-js",
-  "leafgarland/typescript-vim",
-  "lepture/vim-velocity",
-  "lervag/vimtex",
-  "maxmellon/vim-jsx-pretty",
-  "neoclide/jsonc.vim",
-  "nikvdp/ejs-syntax",
-  "othree/html5.vim",
-  "pangloss/vim-javascript",
-  "rhysd/vim-clang-format",
-  "samoshkin/vim-mergetool",
-  "scrooloose/nerdcommenter",
-  {
-    "OXY2DEV/markview.nvim",
-    opts = {
-      preview = {
-        filetypes = { 'markdown', 'codecompanion' },
-        ignore_buftypes = {},
-      }
-    }
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      render_modes = true,
-      sign = {
-        enabled = false,
-      },
-      file_types = { 'markdown', 'codecompanion' },
-    }
-  },
-  "echasnovski/mini.nvim",
-  {
-    "nvim-tree/nvim-tree.lua",
-    lazy = true,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  "stevearc/vim-arduino",
-  "tpope/vim-abolish",
-  "tpope/vim-dotenv",
-  "tpope/vim-fugitive",
-  "tpope/vim-projectionist",
-  "tpope/vim-repeat",
-  "tpope/vim-surround",
-  "uarun/vim-protobuf",
-  "vickenty/vim-hive",
-  "vim-airline/vim-airline",
-  "vim-scripts/svg.vim",
-  "xolox/vim-misc",
-  {
-    "akinsho/bufferline.nvim",
-    config = function()
-      require("bufferline").setup {}
-    end,
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	"Lokaltog/vim-easymotion",
+	"airblade/vim-gitgutter",
+	"alvan/vim-closetag",
+	"artoj/pgn-syntax-vim",
+	"blueyed/vim-diminactive",
+	"chrisbra/csv.vim",
+	"derekwyatt/vim-scala",
+	"ekalinin/Dockerfile.vim",
+	"github/copilot.vim",
+	"hashivim/vim-terraform",
+	"jiangmiao/auto-pairs",
+	"jparise/vim-graphql",
+	{
+		"junegunn/fzf",
+		build = function()
+			vim.fn["fzf#install"]()
+		end,
+	},
+	"junegunn/fzf.vim",
+	"jxnblk/vim-mdx-js",
+	"leafgarland/typescript-vim",
+	"lepture/vim-velocity",
+	"lervag/vimtex",
+	"maxmellon/vim-jsx-pretty",
+	"neoclide/jsonc.vim",
+	"nikvdp/ejs-syntax",
+	"othree/html5.vim",
+	"pangloss/vim-javascript",
+	"rhysd/vim-clang-format",
+	"samoshkin/vim-mergetool",
+	"scrooloose/nerdcommenter",
+	{
+		"OXY2DEV/markview.nvim",
+		opts = {
+			preview = {
+				filetypes = { "markdown", "codecompanion" },
+				ignore_buftypes = {},
+			},
+		},
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			render_modes = true,
+			sign = {
+				enabled = false,
+			},
+			file_types = { "markdown", "codecompanion" },
+		},
+	},
+	"echasnovski/mini.nvim",
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = true,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	"stevearc/vim-arduino",
+	"tpope/vim-abolish",
+	"tpope/vim-dotenv",
+	"tpope/vim-fugitive",
+	"tpope/vim-projectionist",
+	"tpope/vim-repeat",
+	"tpope/vim-surround",
+	"uarun/vim-protobuf",
+	"vickenty/vim-hive",
+	"vim-airline/vim-airline",
+	"vim-scripts/svg.vim",
+	"xolox/vim-misc",
+	{
+		"akinsho/bufferline.nvim",
+		config = function()
+			require("bufferline").setup({})
+		end,
+	},
 })
 
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-  mapping = {
-    -- ... Your other mappings ...
-    ['<CR>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        if luasnip.expandable() then
-          luasnip.expand()
-        else
-          cmp.confirm({
-            select = true,
-          })
-        end
-      else
-        fallback()
-      end
-    end),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
+	mapping = {
+		-- ... Your other mappings ...
+		["<CR>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if luasnip.expandable() then
+					luasnip.expand()
+				else
+					cmp.confirm({
+						select = true,
+					})
+				end
+			else
+				fallback()
+			end
+		end),
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.locally_jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 })
 
-local lsp_cmds = vim.api.nvim_create_augroup('lsp_cmds', { clear = true })
+local lsp_cmds = vim.api.nvim_create_augroup("lsp_cmds", { clear = true })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = lsp_cmds,
-  desc = 'LSP actions',
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      vim.keymap.set(mode, lhs, rhs, { buffer = true })
-    end
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = lsp_cmds,
+	desc = "LSP actions",
+	callback = function()
+		local bufmap = function(mode, lhs, rhs)
+			vim.keymap.set(mode, lhs, rhs, { buffer = true })
+		end
 
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-    bufmap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
-    bufmap({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
-    bufmap('n', '<leader>ta', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    bufmap('n', '[c', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-    bufmap('n', ']c', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  end
+		bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+		bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+		bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+		bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
+		bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+		bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
+		bufmap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+		bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
+		bufmap({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
+		bufmap("n", "<leader>ta", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+		bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+		bufmap("n", "[c", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+		bufmap("n", "]c", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+	end,
 })
